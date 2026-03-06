9522465f4d4a8b71039c580266d9c3207fed7c41
Goto daichi　 <goto.spiqa.design@gmail.com>
2026-03-06 12:24:09 +0900

    404修正

--- 404.php
+++ 404.php
@@ -21,7 +21,7 @@
 
                     <h1 class="el_thanksContainer_ttl">ページが見つかりませんでした</h1>
 
-                    <p class="el_404Container_txt">お探しのページが見つかりませんでした。<br>一時的にアクセスできない状況にあるか、移動もしくは削除された可能性があります。</p>
+                    <p class="el_404Container_txt">お探しのページが見つかりませんでした。<br>一時的にアクセスできない状況にあるか、<br class="is-sp">移動もしくは削除された可能性があります。</p>
 
                     <a href="<?php echo home_url('/'); ?>" class="el_404Container_btn">トップへ戻る</a>
                 </div>
