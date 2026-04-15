099599610a59c13c84752ce31a2bc541f01f2cdc
Goto daichi　 <goto.spiqa.design@gmail.com>
2026-04-15 09:27:03 +0900

    関連ブログ修正

--- blocks/blog-block/blog-block.php
+++ blocks/blog-block/blog-block.php
@@ -1,65 +1,67 @@
 <?php if (is_singular('menu')): ?>
-    <div class="bl_commonBlogSwiperContainer bl_menuBlogSwiperContainer">
-        <div class="bl_commonBlogSwiperContainer_inner">
-            <button class="bl_commonBlogSwiper_prev" type="button">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/arrow-prev.svg" alt="">
-            </button>
+    <?php
+    $relatedBlogList = get_field('related-blog-list'); ?>
+    <?php if (!empty($relatedBlogPosts)): ?>
+        <div class="bl_commonBlogSwiperContainer bl_menuBlogSwiperContainer">
+            <div class="bl_commonBlogSwiperContainer_inner">
+                <button class="bl_commonBlogSwiper_prev" type="button">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/arrow-prev.svg" alt="">
+                </button>
 
-            <div class="swiper bl_commonBlogSwiper">
-                <div class="swiper-wrapper">
-                    <?php
-                    $relatedBlogList = get_field('related-blog-list');
+                <div class="swiper bl_commonBlogSwiper">
+                    <div class="swiper-wrapper">
 
-                    $relatedBlogPosts = get_posts(array(
-                        'post_type' => 'post',
-                        'posts_per_page' => -1,
-                        'post__in' => $relatedBlogList,
-                    ));
-                    ?>
+                        <?php 
+                        $relatedBlogPosts = get_posts(array(
+                            'post_type' => 'post',
+                            'posts_per_page' => -1,
+                            'post__in' => $relatedBlogList,
+                        ));
+                        ?>
 
-                    <?php foreach ($relatedBlogPosts as $relatedBlogPost): ?>
+                        <?php foreach ($relatedBlogPosts as $relatedBlogPost): ?>
 
-                        <div class="swiper-slide">
-                            <article class="bl_commonBlogSlideBtn">
+                            <div class="swiper-slide">
+                                <article class="bl_commonBlogSlideBtn">
+                                    <a href="<?php the_permalink($relatedBlogPost->ID); ?>" class="bl_commonBlogSlideBtn_imgContainer">
+                                        <?php if (has_post_thumbnail($relatedBlogPost->ID)): ?>
+                                            <img class="el_commonBlogSlideBtn_img" src="<?php echo get_the_post_thumbnail_url($relatedBlogPost->ID); ?>" alt="<?php echo get_the_title($relatedBlogPost->ID); ?>">
+                                        <?php else: ?>
+                                            <img class="l_commonBlogSlideBtn_img" src="<?php echo get_template_directory_uri(); ?>/assets/img/common/doctor-no-image.jpg" alt="<?php echo get_the_title($relatedBlogPost->ID); ?>">
+                                        <?php endif; ?>
+                                    </a>
 
-                                <a href="<?php the_permalink($relatedBlogPost->ID); ?>" class="bl_commonBlogSlideBtn_imgContainer">
-                                    <?php if (has_post_thumbnail($relatedBlogPost->ID)): ?>
-                                        <img class="el_commonBlogSlideBtn_img" src="<?php echo get_the_post_thumbnail_url($relatedBlogPost->ID); ?>" alt="<?php echo get_the_title($relatedBlogPost->ID); ?>">
-                                    <?php else: ?>
-                                        <img class="l_commonBlogSlideBtn_img" src="<?php echo get_template_directory_uri(); ?>/assets/img/common/doctor-no-image.jpg" alt="<?php echo get_the_title($relatedBlogPost->ID); ?>">
-                                    <?php endif; ?>
-                                </a>
+                                    <div class="bl_commonBlogSlideBtn_lower">
+                                        <time class="el_commonBlogSlideBtn_lower_time" datetime="<?php echo get_the_date('Y-m-d', $relatedBlogPost->ID); ?>"><?php echo get_the_date('Y.m.d', $relatedBlogPost->ID); ?></time>
 
-                                <div class="bl_commonBlogSlideBtn_lower">
-                                    <time class="el_commonBlogSlideBtn_lower_time" datetime="<?php echo get_the_date('Y-m-d', $relatedBlogPost->ID); ?>"><?php echo get_the_date('Y.m.d', $relatedBlogPost->ID); ?></time>
+                                        <?php
+                                        $categoryList = get_the_category($relatedBlogPost->ID);
+                                        ?>
+                                        <?php if ($categoryList): ?>
+                                            <div class="bl_commonBlogSlideBtn_lower_categoryList">
+                                                <?php foreach ($categoryList as $category): ?>
+                                                    <a href="<?php echo get_term_link($category->term_id); ?>" class="el_commonBlogSlideBtn_lower_categoryList_item"><?php echo $category->name; ?></a>
+                                                <?php endforeach; ?>
+                                            </div>
+                                        <?php endif; ?>
 
-                                    <?php
-                                    $categoryList = get_the_category($relatedBlogPost->ID);
-                                    ?>
-                                    <?php if ($categoryList): ?>
-                                        <div class="bl_commonBlogSlideBtn_lower_categoryList">
-                                            <?php foreach ($categoryList as $category): ?>
-                                                <a href="<?php echo get_term_link($category->term_id); ?>" class="el_commonBlogSlideBtn_lower_categoryList_item"><?php echo $category->name; ?></a>
-                                            <?php endforeach; ?>
-                                        </div>
-                                    <?php endif; ?>
+                                        <h1 class="el_commonBlogSlideBtn_lower_ttl">
+                                            <a class="el_commonBlogSlideBtn_lower_ttl_link" href="<?php the_permalink($relatedBlogPost->ID); ?>">
+                                                <?php echo get_the_title($relatedBlogPost->ID); ?>
+                                            </a>
+                                        </h1>
+                                    </div>
+                                </article>
+                            </div>
 
-                                    <h1 class="el_commonBlogSlideBtn_lower_ttl">
-                                        <a class="el_commonBlogSlideBtn_lower_ttl_link" href="<?php the_permalink($relatedBlogPost->ID); ?>">
-                                            <?php echo get_the_title($relatedBlogPost->ID); ?>
-                                        </a>
-                                    </h1>
-                                </div>
-                            </article>
-                        </div>
-
-                    <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
+                <button class="bl_commonBlogSwiper_next" type="button">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/arrow-next.svg" alt="">
+                </button>
             </div>
-            <button class="bl_commonBlogSwiper_next" type="button">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/arrow-next.svg" alt="">
-            </button>
+            <div class="bl_commonBlogSwiper_pagination"></div>
         </div>
-        <div class="bl_commonBlogSwiper_pagination"></div>
-    </div>
+    <?php endif; ?>
 <?php endif; ?>
